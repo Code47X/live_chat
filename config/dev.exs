9d14cdf0a5c9bd8fe3ac287c642b736a73368185
Tanner Hartwig <tanner.hartwig57@gmail.com>
2021-11-03 19:47:07 -0500

    Setup surface

--- config/dev.exs
+++ config/dev.exs
@@ -54,11 +54,12 @@ config :live_chat, LiveChatWeb.Endpoint,
 
 # Watch static and templates for browser reloading.
 config :live_chat, LiveChatWeb.Endpoint,
+  reloadable_compilers: [:gettext, :elixir, :surface],
   live_reload: [
     patterns: [
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
-      ~r"lib/live_chat_web/(live|views)/.*(ex)$",
+      ~r"lib/live_chat_web/(live|views|components)/.*(ex|sface|js)$",
       ~r"lib/live_chat_web/templates/.*(eex)$"
     ]
   ]
--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule LiveChat.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: [:gettext] ++ Mix.compilers() ++ [:surface],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -48,7 +48,9 @@ defmodule LiveChat.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:surface, "~> 0.6.1"},
+      {:surface_formatter, "~> 0.6.0"}
     ]
   end
 
